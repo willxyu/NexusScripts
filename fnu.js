6f608bd8011a216d54ca9d4347be63215833de16
willxyu <willxyu@users.noreply.github.com>
2019-01-28 15:24:18 +1100

    Update fnu.js

--- fnu.js
+++ fnu.js
@@ -21,3 +21,38 @@ fnu.utils = (function() {
     return ch.repeat(r) + str }
   }
 })()
+
+// Client Overwrites
+if (typeof client != 'undefined') {
+ client.handle_read = function(msg) {
+   var arr = new Uint8Array(msg.data)
+   var s   = ''
+   for (var i = 0; i < arr.length; ++i) { s += String.fromCharCode(arr[i]) }
+   client.read_data(s) }
+  
+ client.read_data = function(s) {
+   var str
+   do {
+     str = client.handle_telnet_read(s); s = '';
+     if (str === false)   { break }
+     if (client.gagged)   { continue }
+     var lines = client.telnet_split(str); lines = client.parse_lines(lines);
+     if (lines === false) { continue }
+     client.current_block = lines;
+     if (client.triggers_enabled) { lines = client.handle_triggers(lines) }
+     client.handle_beep_code(lines)
+     for (var idx = 0; idx < lines.length; ++idx) {
+       var temp = '';
+       if (lines[idx].line) { temp = lines[idx].line }
+       else if (lines[idx].prompt) { temp = lines[idx].prompt }
+       if (temp.length) { client.on_msg_recv(temp) }
+     }
+     client.run_function('onBlock', null, 'ALL')
+     client.display_text_block(lines)
+     client.current_line = undefined; client.current_block = undefined;
+   } while (str !== false)
+ }
+}
+
+if (typeof ws != 'undefined') { ws.onmessage = client.handle_read }
+
